4e387429d05cc06f4401ee72b4e95ff0e818b175
Mario Lopez <mario@techmunchies.net>
2023-09-12 19:45:29 -0400

    small nvim config, fix macos fisher

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,4 +1,10 @@
--- Check if running neovim or vscode-neovim
+-- set initial config
+vim.g.mapleader = " "
+
+require 'plugins'.setup()
+require 'keybindings.shared'.setup()
+
+-- Check if running neovim or vscode-neovim and applies custom configuration
 if not vim.g.vscode then
     -- TBD
 else
