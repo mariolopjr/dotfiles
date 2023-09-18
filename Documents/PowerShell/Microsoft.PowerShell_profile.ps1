24bc5c2e82a7e08a10b56c4a20b44632c482286a
Mario Lopez <mario@techmunchies.net>
2023-09-18 13:24:00 -0400

    tweaks for wsl packages, add envar to ps

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,22 @@
+# Add to path
+Function Add-ToPathVariable {
+    Param(
+        [string] $NewPath
+    )
+
+    If (!(Test-Path $NewPath)) {
+        Write-Warning "$NewPath does not exist"
+        Return
+    }
+
+    $NewPathRegex = [regex]::Escape($NewPath)
+    $CurrentPath = $env:Path
+    $CurrentPathArray = $CurrentPath.Split(";") | Where-Object { $_ -notmatch "^$NewPathRegex\\?" }
+    $env:Path = ($CurrentPathArray + $NewPath) -join ";"
+}
+
+Add-ToPathVariable "C:\Users\mario\AppData\Roaming\Python\Python311\Scripts"
+
 # Disable powershell telemetry
 $env:POWERSHELL_TELEMETRY_OPTOUT = $true
 
