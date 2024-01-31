1b97871f1fd46b996d000ef5a6e37268e9301192
Mario Lopez <mario@techmunchies.net>
2024-01-31 18:26:02 -0500

    tweaks to external files, godot, vim

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,5 +1,11 @@
 -- set initial config
 vim.g.mapleader = " "
 
+-- check if running in vscode
+if vim.g.vscode then
+    -- https://github.com/vscode-neovim/vscode-neovim/issues/298
+    vim.opt.clipboard:append("unnamedplus")
+end
+
 require 'plugins'.setup()
 require 'keybindings.shared'.setup()
