0a608233890ac01df66f59eff79faee6a576e30b
Mario Lopez <mario@techmunchies.net>
2024-02-28 18:48:31 -0500

    configure nvim with lazy

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,11 +1,22 @@
 -- set initial config
 vim.g.mapleader = " "
 
+require 'plugins'.setup()
+require 'keybindings.shared'.setup()
+
 -- TODO: move to a better place, potentially a separate file for vscode-specific configuration
 if vim.g.vscode then
     -- https://github.com/vscode-neovim/vscode-neovim/issues/298
     vim.opt.clipboard:append("unnamedplus")
-end
 
-require 'plugins'.setup()
-require 'keybindings.shared'.setup()
+    local function print_plugins()
+        local plugins = require("lazy").plugins()
+        for _, plugin in pairs(plugins) do
+            if plugin.name ~= nil then
+                print(plugin.name)
+            end
+        end
+    end
+    print_plugins()
+    print("VSCode specific configuration loaded")
+end
