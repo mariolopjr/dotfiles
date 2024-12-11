efc8c49045c00606181e66580562bf95416d702c
Mario Lopez <mario@techmunchies.net>
2024-12-10 20:55:02 -0500

    search with hidden files enabled

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -89,6 +89,11 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Shortcut for searching files including hidden files
+      vim.keymap.set('n', '<leader>s,', function()
+        builtin.find_files { hidden = true }
+      end, { desc = '[S]earch [,] Files including hidden' })
     end,
   },
 }
