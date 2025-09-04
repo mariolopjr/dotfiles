7825ee8020b3e9ffd80735049107cefd2ed6aca1
Mario Lopez <mario@techmunchies.net>
2025-09-04 19:51:52 -0400

    blink vim.pack

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,3 +1,84 @@
+local M = {}
+
+M.setup = function()
+  -- install
+  vim.pack.add({
+    { src = "https://github.com/xzbdmw/colorful-menu.nvim" },
+    { src = "https://github.com/saghen/blink.cmp" },
+  })
+
+  -- options
+  local colorful_menu = require("colorful-menu")
+  ---@type blink.cmp.Config
+  local opts = {
+    keymap = { preset = "default" },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    signature = { enabled = true },
+    sources = {
+      default = { "lazydev", "lsp", "path", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
+      },
+    },
+
+    -- add colorful-menu to blink
+    completion = {
+      -- Show documentation when selecting a completion item
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+      menu = {
+        draw = {
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              width = { fill = true, max = 60 },
+              text = function(ctx)
+                local highlights_info = colorful_menu.blink_highlights(ctx)
+                if highlights_info ~= nil then
+                  return highlights_info.label
+                else
+                  return ctx.label
+                end
+              end,
+              highlight = function(ctx)
+                local highlights = {}
+                local highlights_info = colorful_menu.blink_highlights(ctx)
+                if highlights_info ~= nil then
+                  highlights = highlights_info.highlights
+                end
+                for _, idx in ipairs(ctx.label_matched_indices) do
+                  table.insert(
+                    highlights,
+                    { idx, idx + 1, group = "BlinkCmpLabelMatch" }
+                  )
+                end
+                return highlights
+              end,
+            },
+          },
+        },
+      },
+    },
+  }
+
+  -- local opts_extend = { "sources.default" }
+
+  -- setup
+  local blink = require("blink-cmp")
+  blink.setup(opts)
+end
+
+if vim.g.use_vim_pack then
+  return M
+end
+
 return {
   {
     "saghen/blink.cmp",
@@ -37,7 +118,8 @@ return {
               label = {
                 width = { fill = true, max = 60 },
                 text = function(ctx)
-                  local highlights_info = require("colorful-menu").blink_highlights(ctx)
+                  local highlights_info =
+                    require("colorful-menu").blink_highlights(ctx)
                   if highlights_info ~= nil then
                     return highlights_info.label
                   else
@@ -46,12 +128,16 @@ return {
                 end,
                 highlight = function(ctx)
                   local highlights = {}
-                  local highlights_info = require("colorful-menu").blink_highlights(ctx)
+                  local highlights_info =
+                    require("colorful-menu").blink_highlights(ctx)
                   if highlights_info ~= nil then
                     highlights = highlights_info.highlights
                   end
                   for _, idx in ipairs(ctx.label_matched_indices) do
-                    table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+                    table.insert(
+                      highlights,
+                      { idx, idx + 1, group = "BlinkCmpLabelMatch" }
+                    )
                   end
                   return highlights
                 end,
