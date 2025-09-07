4ed05c8ae4e0875e6edb2a78e2cff63ade36a986
Mario Lopez <mario@techmunchies.net>
2025-09-06 23:29:30 -0400

    fix lsp

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 local icons = {
   [vim.diagnostic.severity.ERROR] = "",
   [vim.diagnostic.severity.WARN] = "",
@@ -5,29 +7,33 @@ local icons = {
   [vim.diagnostic.severity.INFO] = "",
 }
 
--- enable LSPs
-vim.lsp.enable({
-  "clangd",
-  "fish_lsp",
-  "gopls",
-  "lua_ls",
-  "ty",
-})
+M.setup = function()
+  -- enable LSPs
+  vim.lsp.enable({
+    "clangd",
+    "fish_lsp",
+    "gopls",
+    "lua_ls",
+    "ty",
+  })
 
--- setup inlay hints
-vim.lsp.inlay_hint.enable(true)
+  -- setup inlay hints
+  vim.lsp.inlay_hint.enable(true)
 
--- setup inline diagnostics
-vim.diagnostic.config({
-  underline = true,
-  update_in_insert = false,
-  virtual_text = {
-    prefix = function(diagnostics)
-      return icons[diagnostics.severity] or "●"
-    end,
-    spacing = 4,
-    source = "if_many", -- show source if multiple
-  },
-  signs = icons,
-  severity_sort = true,
-})
+  -- setup inline diagnostics
+  vim.diagnostic.config({
+    underline = true,
+    update_in_insert = false,
+    virtual_text = {
+      prefix = function(diagnostics)
+        return icons[diagnostics.severity] or "●"
+      end,
+      spacing = 4,
+      source = "if_many", -- show source if multiple
+    },
+    signs = icons,
+    severity_sort = true,
+  })
+end
+
+return M
