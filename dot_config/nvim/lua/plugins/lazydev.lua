224563e655d6eb5cb334ec85d3260864ab5648ba
Mario Lopez <mario@techmunchies.net>
2025-09-07 13:18:39 -0400

    add love2d lib

--- dot_config/nvim/lua/plugins/lazydev.lua
+++ dot_config/nvim/lua/plugins/lazydev.lua
@@ -1,6 +1,7 @@
 ---@module "lazydev"
 return {
   { "justinsgithub/wezterm-types", lazy = true },
+  { "LuaCATS/love2d", lazy = true },
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
@@ -11,6 +12,7 @@ return {
       library = {
         { plugins = { "nvim-dap-ui" }, types = true },
         { path = "wezterm-types", mods = { "wezterm" } },
+        { path = "love2d/library", words = { "love" } },
       },
     },
   },
