9fbb057228b6644ff0f97f60b0527cc48f845096
Mario Lopez <mariolopjr@users.noreply.github.com>
2025-10-28 18:43:29 -0400

    add alejandra

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -61,6 +61,7 @@ return {
         html = { "prettier" },
         xml = { "yq" },
         yaml = { "yq " },
+        nix = { "alejandra" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
